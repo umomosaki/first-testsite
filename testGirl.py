a3b4ec48912afac634823861af0fbaf636f5e180
momosaki <u-momo@pound4pound.biz>
2020-02-17 10:14:03 +0900

    my Django Girls ap 1st commit

---
+++ testGirl.py
@@ -0,0 +1,9 @@
+log_name = input("Please input your name!!")
+
+def login_chk(name):
+    if name == "Yu":
+        print("Hello! you\'re logged in.")
+    else:
+        print("Check your name?")
+
+login_chk (log_name)
